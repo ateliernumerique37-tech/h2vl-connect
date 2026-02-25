4a6b6e59e3c76a2eb93089a96751e2f837686ffe
atelier numérique37 <ateliernumerique37@gmail.com>
2026-02-25 10:29:30 +0000

    ce que tu vas faire. tu vas supprimer les fichiers de configuration de f

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   projectId: "studio-6079106449-cf583",
   appId: "1:574767759997:web:a098dcd67797dde00b2714",
@@ -14,7 +12,7 @@ const firebaseConfig = {
   storageBucket: "studio-6079106449-cf583.appspot.com"
 };
 
-// Initialize Firebase
+// Initialisation sécurisée pour Next.js (pour éviter que l'app ne s'initialise deux fois)
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
