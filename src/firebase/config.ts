623b906a03dc7d9e94ef60087bb0ab6aad509dac
atelier numérique37 <ateliernumerique37@gmail.com>
2026-02-25 10:42:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6079106449-cf583",
+  "appId": "1:574767759997:web:a098dcd67797dde00b2714",
+  "apiKey": "AIzaSyCuDzDd6YMZhKa20nl7jxfbKbBfR4gsalY",
+  "authDomain": "studio-6079106449-cf583.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "574767759997"
+};
